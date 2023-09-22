a0b617331413356affef1a11cf78120ebf67e003
2016bits <1835154145@qq.com>
2023-09-22 08:51:30 +0800

    利用Aquila根据claim和ground_truth evidence直接生成判断结果

--- scripts/utils/flagai_model.py
+++ scripts/utils/flagai_model.py
@@ -32,3 +32,17 @@ class FlagAIModel:
         # remove excess generated parts from the tail
         res = res.split('\n\n')[0]
         return res
+    
+    def generate_answer(self, prompt, temperature=0.0, top_p=1.0):
+        input_ids = self.tokenizer.encode_plus_non_glm(prompt)["input_ids"][:-1]
+        input_len = len(input_ids)
+
+        max_length = input_len + self.max_new_tokens
+        with torch.no_grad():
+            res = self.predictor.predict_generate_randomsample(
+                prompt, out_max_length=max_length, top_p=top_p, temperature=temperature
+            )
+        # # remove excess generated parts from the tail
+        # res = res.split()
+        # return res[0] if res[0] else res[1]
+        return res
---
+++ scripts/verify_with_aquila.py
@@ -0,0 +1,199 @@
+import argparse
+import torch
+import json
+import sqlite3
+import random
+from tqdm import tqdm
+from sklearn.metrics import classification_report, confusion_matrix
+
+from utils import log, flagai_model
+
+label_map = {
+    'no': 0,
+    'false': 0,
+    'yes': 1,
+    'true': 1,
+    'unknown': 2,
+    'un': 2
+}
+
+class Program_Generate:
+    def __init__(self, args, logger):
+        self.args = args
+        self.dataset_name = args.dataset_name
+        self.num_programs_per_example = args.num_programs_per_example
+        self.db_table = args.db_table
+
+        conn = sqlite3.connect(args.db_path)
+        self.c = conn.cursor()
+        logger.info("connect to database successfully...")
+
+        self.ai_model = flagai_model.FlagAIModel(args)
+        logger.info("load model successfully...")
+
+    def convert_evidence(self, evidence_list):
+        evidence_text = ""
+        for title, line_id in evidence_list:
+            sql = """select * from {} where id = "{}";""".format(self.db_table, title)
+            cursor = self.c.execute(sql)
+            for row in cursor:
+                lines = row[2].split('\n')
+                for line in lines:
+                    sent_id = eval(line.split('\t')[0])
+                    if sent_id == line_id:
+                        sent_text = line.replace('{}\t'.format(sent_id), '')
+                        evidence_text += sent_text
+        return evidence_text
+    
+    def evaluate(self, predictions, ground_truth, logger, num_of_classes=2):
+        if num_of_classes == 2:
+            target_names = ['REFUTES', 'SUPPORTS']
+            label_map = {'REFUTES': 0, 'SUPPORTS': 1}
+            gold = [label_map[e] for e in ground_truth]
+            pred = [label_map[e] for e in predictions]
+            logger.info(classification_report(gold, pred, labels=range(2), target_names=target_names, digits=4))
+            logger.info(confusion_matrix(gold, pred))
+        elif num_of_classes == 3:
+            target_names = ['REFUTES', 'SUPPORTS', 'NOT ENOUGH INFO']
+            label_map = {'REFUTES': 0, 'SUPPORTS': 1, 'NOT ENOUGH INFO': 2}
+            gold = [label_map[e] for e in ground_truth]
+            pred = [label_map[e] for e in predictions]
+            logger.info(classification_report(gold, pred, labels=range(3), target_names=target_names, digits=4))
+            logger.info(confusion_matrix(gold, pred))
+    
+    def batch_generate_program(self, logger, datasets):
+        # generate programs
+        temperature = 0.0 if self.num_programs_per_example == 1 else 0.7
+        results = []
+
+        predictions = []
+        targets = []
+        logger.info("Start predicting...")
+        
+        for inst in tqdm(datasets):
+            claim = inst['claim']
+            evidence = self.convert_evidence(inst['evidence'])
+            target = inst['label']
+            targets.append(target)
+
+            # create prompt
+            input_text = f"{evidence}\nBased on the above information, is it true that {claim}? true, false or unknown? The answer is: "
+            if len(input_text) > self.args.max_aquila_tokens:
+                evidence_len = self.args.max_aquila_tokens - len(claim) - 25
+                input_text = f"{evidence[:evidence_len]}\nBased on the above information, is it true that {claim}? true, false or unknown? The answer is: "
+
+            pred = self.ai_model.generate_answer(input_text, temperature)
+            flag = False
+            out = pred.split()
+            if out[0]:
+                if out[0] in label_map:
+                    pred = label_map[out[0]]
+                    flag = True
+            else:
+                if out[1] in label_map:
+                    pred = label_map[out[1]]
+                    flag = True
+
+            for _label in label_map:
+                if flag:
+                    break
+                if _label in pred.lower():
+                    pred = label_map[_label]
+                    flag = True
+                    break
+            if not flag:
+                logger.info("Alert! prediction error id:{}, prediction: {}".format(inst['id'], pred))
+                pred = random.sample([0, 1, 2], 1)[0]
+            # except:
+            #     pred = random.sample([0, 1, 2], 1)[0]
+            #     logger.info('Error in predicting for example: {}'.format(inst['id']))
+        
+            # # map output to label_map
+            # pred = pred.lower().strip()
+            # label_map = {'true': 1, 'false': 0, 'yes': 1, 'no': 0, 'unknown': 2, 'un': 2, "it is not possible to tell": 2,
+            #              "it's impossible to say": 2, 'not enough info': 2, 'not enough information': 2}
+            # if pred in label_map:
+            #     pred = label_map[pred]
+            # else:
+            #     logger.info("Alert! prediction error id:{}, prediction: {}".format(inst['id'], pred))
+            #     pred = random.sample([0, 1, 2], 1)[0]
+            
+            if pred == 0:
+                pred_label = 'REFUTES'
+            elif pred == 1:
+                pred_label = 'SUPPORTS'
+            elif pred == 2:
+                pred_label = 'NOT ENOUGH INFO'
+            predictions.append(pred_label)
+            
+            results.append({
+                'id': inst['id'],
+                'claim': inst['claim'],
+                'label': inst['label'],
+                'prediction': pred_label
+            })
+
+        # evaluate
+        self.evaluate(predictions, targets, logger, 3)
+
+        return results
+
+def main(args):
+    log_path = args.log_path + args.dataset_name + '_verify_with_aquila.log'
+
+    # init logger
+    logger = log.get_logger(log_path)
+    logger.info(args)
+
+    # load data
+    logger.info("loading data......")
+    data_path = args.data_path + args.dataset_name + "/processed/" + args.mode + ".json"
+    with open(data_path, 'r') as f:
+        dataset = json.load(f)
+    dataset = dataset if args.num_eval_samples < 0 else dataset[:args.num_eval_samples]
+
+    # load model
+    logger.info("loading model......")
+    program_generate = Program_Generate(args, logger)
+    outputs = program_generate.batch_generate_program(logger, dataset)
+
+    # save outputs
+    save_path = args.save_path + args.dataset_name + "_test_verify_with_aquila_results.json"
+    with open(save_path, 'w') as f:
+        json.dump(outputs, f, indent=2, ensure_ascii=False)
+    logger.info("Finished!")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # data
+    parser.add_argument('--log_path', type=str, default='./logs/')
+    parser.add_argument('--data_path', type=str, default='./data/')
+    parser.add_argument('--dataset_name', type=str, default='FEVER', choices=['SCIFACT'])
+    parser.add_argument('--save_path', type=str, default='./results/ablation/')
+    parser.add_argument('--mode', type=str, default='test', choices=['train', 'dev', 'test'])
+
+    parser.add_argument('--num_eval_samples', type=int, default=-1)
+    parser.add_argument('--num_programs_per_example', type=int, default=1)
+
+    parser.add_argument('--use_cuda', type=bool, default=True)
+    parser.add_argument('--gpu', type=int, default=0)
+
+    # wikipedia arguments
+    parser.add_argument('--db_path', type=str, default='./data/Wikipedia/data/wikipedia.db')
+    parser.add_argument('--db_table', type=str, default='documents')
+
+    # model
+    parser.add_argument('--max_aquila_tokens', type=int, default=2048)
+    parser.add_argument('--max_new_tokens', type=int, default=8)
+    parser.add_argument('--task_name', type=str, default='lm')
+    parser.add_argument('--model_name', type=str, default='aquilacode-7b-nv')
+    parser.add_argument('--use_cache', type=bool, default=True)
+    parser.add_argument('--fp16', type=bool, default=True)
+    parser.add_argument('--model_path', type=str, default='../checkpoints_in/')
+    
+    args = parser.parse_args()
+
+    if torch.cuda.is_available() and args.use_cuda:
+        torch.cuda.set_device(int(args.gpu))
+    
+    main(args)
